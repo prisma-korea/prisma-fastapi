50a6b2ae2c251db2a54fe3c20db0707fe6a2c233
hyochan <dooboolab@gmail.com>
2022-06-12 19:47:18 +0900

    Install prisma client

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from src.apis import apis
 from typing import Union
+from prisma import Prisma
 
 from fastapi import FastAPI
+
+prisma = Prisma()
 
 app = FastAPI()
 # app.add_middleware(GZipMiddleware, minimum_size=1000)
 app.include_router(apis, prefix='/apis')
 
+@app.on_event("startup")
+async def startup():
+    await prisma.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await prisma.disconnect()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World1111"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def read_root():
+    users = await prisma.user.find_many();
+    return {"users": users}
